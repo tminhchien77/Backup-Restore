c8842661beb3e1b7699f0465214508f02e77402a
tminhchien77 <tminhchien77@gmail.com>
2022-04-15 11:13:04 +0700

    Done btnRestoreAtTime

---
+++ FormRestoreAtTime.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace backup_restore
+{
+    partial class FormRestoreAtTime
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormRestoreAtTime));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.dtpTime = new System.Windows.Forms.DateTimePicker();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblTime);
+            this.groupBox1.Controls.Add(this.dtpTime);
+            this.groupBox1.Controls.Add(this.lblDate);
+            this.groupBox1.Controls.Add(this.dtpDate);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(407, 83);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thời điểm phục hồi";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(227, 40);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(23, 13);
+            this.lblTime.TabIndex = 3;
+            this.lblTime.Text = "Giờ";
+            // 
+            // dtpTime
+            // 
+            this.dtpTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpTime.Location = new System.Drawing.Point(265, 34);
+            this.dtpTime.Name = "dtpTime";
+            this.dtpTime.ShowUpDown = true;
+            this.dtpTime.Size = new System.Drawing.Size(119, 20);
+            this.dtpTime.TabIndex = 2;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(13, 40);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(32, 13);
+            this.lblDate.TabIndex = 1;
+            this.lblDate.Text = "Ngày";
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDate.Location = new System.Drawing.Point(51, 34);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(119, 20);
+            this.dtpDate.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(0, 83);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(407, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Hướng dẫn";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 16);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(401, 81);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Thời điểm phục hồi phải sau thời điểm sao lưu của bản sao lưu đã chọn và trước th" +
+    "ời điểm xảy ra lỗi ít nhất 3 phút.\n";
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.FlatAppearance.BorderSize = 0;
+            this.btnRestore.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRestore.ForeColor = System.Drawing.Color.Black;
+            this.btnRestore.Image = ((System.Drawing.Image)(resources.GetObject("btnRestore.Image")));
+            this.btnRestore.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRestore.Location = new System.Drawing.Point(149, 186);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(112, 48);
+            this.btnRestore.TabIndex = 3;
+            this.btnRestore.Text = "Phục hồi";
+            this.btnRestore.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRestore.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnRestore.UseVisualStyleBackColor = true;
+            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+            // 
+            // FormRestoreAtTime
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 236);
+            this.Controls.Add(this.btnRestore);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormRestoreAtTime";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "FormRestoreAtTime";
+            this.Load += new System.EventHandler(this.FormRestoreAtTime_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.DateTimePicker dtpTime;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnRestore;
+    }
+}
---
+++ FormRestoreAtTime.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace backup_restore
+{
+    public partial class FormRestoreAtTime : Form
+    {
+        public int positionBackup;
+        public DateTime datetimeBackup;
+        public FormRestoreAtTime()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        void restoreAtTime()
+        {
+            String str = "USE master \n" +
+                "ALTER DATABASE " + Program.database + " SET SINGLE_USER WITH ROLLBACK IMMEDIATE \n" +
+                "BACKUP LOG " + Program.database + " TO DISK = '" + Program.defaultPath + "/" + Program.database + ".trn' WITH NORECOVERY \n" +
+                "RESTORE DATABASE " + Program.database + " FROM " + "DEVICE_" + Program.database + " WITH FILE=" + positionBackup + ", NORECOVERY\n" +
+                "RESTORE DATABASE " + Program.database + " FROM DISK = '" + Program.defaultPath + "/" + Program.database + ".trn' WITH STOPAT='" + dtpDate.Value.ToString("yyyy-MM-dd") + " " + dtpTime.Value.TimeOfDay.ToString() + "'\n" +
+                "ALTER DATABASE " + Program.database + "  SET MULTI_USER";
+            try
+            {
+                if(Program.execNonQuery(str, "Lỗi restore dữ liệu!") > 0)
+                {
+                    MessageBox.Show("Thành công");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi tạo backup device\n" + ex.Message, "Thông báo lỗi", MessageBoxButtons.OK);
+                return;
+            }
+        }
+
+        private void btnRestore_Click(object sender, EventArgs e)
+        {
+            if (dtpDate.Value.Date == datetimeBackup.Date && dtpTime.Value.TimeOfDay.Ticks < datetimeBackup.TimeOfDay.Ticks)
+            {
+                MessageBox.Show("Thời điểm phục hồi phải sau thời điểm sao lưu: " + datetimeBackup.ToString("dd/MM/yyyy HH:mm:ss"), "", MessageBoxButtons.OK);
+                return ;
+            }
+
+            if (dtpDate.Value.Date == DateTime.Now.Date && dtpTime.Value.TimeOfDay.Ticks >= DateTime.Now.AddMinutes(-3).TimeOfDay.Ticks)
+            {
+                MessageBox.Show("Thời điểm phục hồi phải trước thời điểm hiện tại.", "", MessageBoxButtons.OK);
+                return;
+            }
+            using (FormWait frm=new FormWait(restoreAtTime))
+            {
+                this.Hide();
+                frm.ShowDialog(this);
+            }
+        }
+
+        private void FormRestoreAtTime_Load(object sender, EventArgs e)
+        {
+            dtpDate.MinDate = datetimeBackup.Date;
+            dtpDate.Value = datetimeBackup.Date;
+            dtpTime.Value = datetimeBackup;
+        }
+    }
+}
